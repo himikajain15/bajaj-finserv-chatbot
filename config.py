aaf85131434ac2c8a6f83eaa1e15804c8bd88d1d
Himika Jain <himikajain2110@gmail.com>
2025-07-12 15:14:49 +0530

    🚀 Re-pushing cleaned project files

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_PATH = "data/"
+TRANSCRIPT_PATH = os.path.join(DATA_PATH, "transcripts/")
+PRICE_CSV = os.path.join(DATA_PATH, "BFS_Share_Price.csv")
+VECTOR_DB_PATH = "vectorstore/index"
