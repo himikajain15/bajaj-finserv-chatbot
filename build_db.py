80446669f466f1b17efd110ca60ba4c290d2fb82
Himika Jain <himikajain2110@gmail.com>
2025-07-12 15:10:51 +0530

    🔥 Reset: Remove all files except .gitignore

--- build_db.py
+++
@@ -1,4 +0,0 @@
-from modules.transcript_qa import build_vectorstore
-
-build_vectorstore()
-print("Vectorstore built successfully.")
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DATA_PATH = "data/"
-TRANSCRIPT_PATH = os.path.join(DATA_PATH, "transcripts/")
-PRICE_CSV = os.path.join(DATA_PATH, "BFS_Share_Price.csv")
-VECTOR_DB_PATH = "vectorstore/index"
